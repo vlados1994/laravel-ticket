16f22396bc2274aeb86919fc7e8a4970ba91cdc8
Alexander Orlovsky <w5www@ya.ru>
2017-04-01 14:00:54 +0700

    idk what i ve changed

---
+++ resources/views/shared/admin-navbar.blade.php
@@ -0,0 +1,26 @@
+
+<li class="dropdown"> 
+
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> Настройки 
+        <span class="caret"></span>
+    </a> 
+    <ul class="dropdown-menu" role="menu"> 
+        <li> <a href="{{route('routing')}}"> Правила роутинга</a> </li>
+        <li> <a href="#"> URL Rewrite </a> </li>
+    </ul> 
+
+</li>
+
+<li class="dropdown"> 
+
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> Модули 
+        <span class="caret"></span>
+    </a> 
+    <ul class="dropdown-menu" role="menu"> 
+        <li> <a href="{{route('routing')}}"> Catalog</a> </li>
+    </ul> 
+
+</li>
+
+<li><a href="">Пользователи</a></li>
+<li><a href="">Система</a></li>
